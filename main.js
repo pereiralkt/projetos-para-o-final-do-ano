f984f921e614de2f81df9d008bfcdaa7f57a81ce
pereiralkt <pereira.filho.paulo@escola.pr.gov.br>
2026-05-12 09:13:34 -0300

    tgtdy4

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2026-10-05T34:23:00");
+const tempoObjetivo2 = new Date("2026-12-05T14:89:00");
+const tempoObjetivo3 = new Date("2026-12-30T12:83:00");
+const tempoObjetivo4 = new Date("2025-02-01T27:09:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
